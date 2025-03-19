ad1d14aeaa8025df577a3093b795245dddc95506
bshrestha432 <bshrestha@rrc.ca>
2025-03-18 22:49:26 -0500

    Add pagination for coins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def index
+    if params[:search]
+      @coins = Coin.where("name LIKE ?", "%#{params[:search]}%")
+    else
+      @coins = Coin.all
+    end
+  end
 end
--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,7 +1,8 @@
 class CoinsController < ApplicationController
   def index
+    if params[:search]
+      @coins = Coin.where("name LIKE ?", "%#{params[:search]}%").page(params[:page])
+    else
+      @coins = Coin.page(params[:page])
+    end
   end
-
-  def show
-  end
-end
